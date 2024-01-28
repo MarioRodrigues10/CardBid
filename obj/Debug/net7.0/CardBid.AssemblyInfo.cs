0202dd28c91d23b1bc101a1b32d8739f59f5d191
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-01-28 22:39:06 +0000

    feat: add images to main page

--- obj/Debug/net7.0/CardBid.AssemblyInfo.cs
+++ obj/Debug/net7.0/CardBid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardBid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ed2f9206e1815334bdfdc8bcc00498d3b19723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ac11a9a69170646c9c88ac491c73c14dc6e022")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardBid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardBid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
